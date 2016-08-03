5fc7d28f5417d9206f1cf6eac1bab6644634cc77
David Xie <mr.david.xie@gmail.com>
2016-08-03 01:26:02 -0700

    now uses the supplied videos

--- app/src/main/java/com/example/davidxie/esieassess/ResultsActivity.java
+++ app/src/main/java/com/example/davidxie/esieassess/ResultsActivity.java
@@ -1,7 +1,9 @@
 package com.example.davidxie.esieassess;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -16,5 +18,13 @@ public class ResultsActivity extends AppCompatActivity {
         ImageView iv = (ImageView)findViewById(R.id.imageView2);
         assert iv != null;
         Picasso.with(this).load(R.drawable.excellent).fit().centerCrop().into(iv);
+
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ResultsActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/davidxie/esieassess/VideoActivity.java
+++ app/src/main/java/com/example/davidxie/esieassess/VideoActivity.java
@@ -6,20 +6,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.media.CamcorderProfile;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.WindowManager;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Created by davidxie on 7/15/16.
@@ -34,6 +41,7 @@ public class VideoActivity extends AppCompatActivity {
     private MediaRecorder mMediaRecorder;
     private File mOutputFile;
     private Boolean isRecording;
+    private ArrayList<Integer> videoArray;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,55 +53,92 @@ public class VideoActivity extends AppCompatActivity {
 
         myVideoView = (VideoView) findViewById(R.id.video_view);
 
+//
+//        videoProgressDialog = new ProgressDialog(VideoActivity.this);
+//        videoProgressDialog.setTitle("Preparing ");
+//        videoProgressDialog.setMessage("Loading...");
+//        videoProgressDialog.setCancelable(false);
+//        videoProgressDialog.show();
 
-        videoProgressDialog = new ProgressDialog(VideoActivity.this);
-        videoProgressDialog.setTitle("Preparing ");
-        videoProgressDialog.setMessage("Loading...");
-        videoProgressDialog.setCancelable(false);
-        videoProgressDialog.show();
+        videoArray = new ArrayList<>();
+        videoArray.add(R.raw.road_s0);
+        videoArray.add(R.raw.s1);
+        videoArray.add(R.raw.s2);
+        videoArray.add(R.raw.s3);
+        videoArray.add(R.raw.s4);
+        videoArray.add(R.raw.s5);
+        videoArray.add(R.raw.s6);
+        videoArray.add(R.raw.s7);
 
-        try {
-            myVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbit));
-        } catch (Exception e) {
-            Log.e(TAG, e.getMessage());
-            e.printStackTrace();
-        }
+        videoArray.remove(1 + (int)(Math.random() * ((videoArray.size() - 2) + 1)));
+        videoArray.remove(1 + (int)(Math.random() * ((videoArray.size() - 2) + 1)));
+        videoArray.remove(1 + (int)(Math.random() * ((videoArray.size() - 2) + 1)));
+        videoArray.remove(1 + (int)(Math.random() * ((videoArray.size() - 2) + 1)));
+
+        playVideo();
 
         myVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             public void onPrepared(MediaPlayer mediaPlayer) {
                 // RXIE: Camera code caused crashes, take it out for now
-                // new MediaPrepareTask().execute(null, null, null);
-                videoProgressDialog.dismiss();
+//                // new MediaPrepareTask().execute(null, null, null);
+//                try {
+//                    setUpVideo();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                videoProgressDialog.dismiss();
                 Log.d(TAG, "start playing video now");
                 myVideoView.start();
-
             }
         });
 
         myVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             public void onCompletion(MediaPlayer mediaPlayer) {
-                Log.d(TAG, "video played");
-                Intent intent = new Intent(VideoActivity.this, ResultsActivity.class);
-                startActivity(intent);
+                if (videoArray.size() < 1) {
+                    Log.d(TAG, "video played");
+//                mCamera.release();
+//                mMediaRecorder.stop();
+                    Intent intent = new Intent(VideoActivity.this, ResultsActivity.class);
+                    startActivity(intent);
+                } else {
+                    playVideo();
+                }
             }
         });
     }
 
-    private void releaseMediaRecorder(){
-        if (mMediaRecorder != null) {
-            // clear recorder configuration
-            mMediaRecorder.reset();
-            // release the recorder object
-            mMediaRecorder.release();
-            mMediaRecorder = null;
-            // Lock camera for later use i.e taking it back from MediaRecorder.
-            // MediaRecorder doesn't need it anymore and we will release it if the activity pauses.
-            mCamera.lock();
+    private void playVideo() {
+        if (videoArray.size() > 0) {
+            myVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + videoArray.get(0)));
+            videoArray.remove(0);
+        } else {
+            Intent intent = new Intent(VideoActivity.this, ResultsActivity.class);
+            startActivity(intent);
         }
     }
 
+    private String getPath() {
+        File sdDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        if (!sdDir.exists() && !sdDir.mkdirs()) {
+            Log.d(TAG, "Can't create directory to save image.");
+            Toast.makeText(VideoActivity.this, "Can't create directory to save image.",
+                    Toast.LENGTH_LONG).show();
+            return null;
+        }
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyymmddhhmmss");
+        String date = dateFormat.format(new Date());
+        String photoFile = "Picture_" + date + ".jpg";
+
+        String filename = sdDir.getPath() + File.separator + photoFile;
+
+        File pictureFile = new File(filename);
+        return filename;
+    }
+
     private int findFrontFacingCamera() {
-        int cameraId = -1;
+        int cameraId = 0;
+        // Search for the front facing camera
         int numberOfCameras = Camera.getNumberOfCameras();
         for (int i = 0; i < numberOfCameras; i++) {
             Camera.CameraInfo info = new Camera.CameraInfo();
@@ -105,75 +150,27 @@ public class VideoActivity extends AppCompatActivity {
         }
         return cameraId;
     }
-
-    private boolean hasCamera(Context context) {
-        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
-    }
-
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    private boolean prepareVideoRecorder(){
-
-        // BEGIN_INCLUDE (configure_preview)
-        mCamera = CameraHelper.getDefaultCameraInstance();
-        // END_INCLUDE (configure_preview)
-
-
-        // BEGIN_INCLUDE (configure_media_recorder)
-        mMediaRecorder = new MediaRecorder();
-
-        // Step 1: Unlock and set camera to MediaRecorder
-        mCamera.unlock();
-        mMediaRecorder.setCamera(mCamera);
-        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT );
-        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-        mOutputFile = CameraHelper.getOutputMediaFile(CameraHelper.MEDIA_TYPE_VIDEO);
-        if (mOutputFile == null) {
-            return false;
-        }
-        mMediaRecorder.setOutputFile(mOutputFile.getPath());
-        // END_INCLUDE (configure_media_recorder)
-
-        // Step 5: Prepare configured MediaRecorder
+    
+    private void setUpVideo() throws IOException {
         try {
-            mMediaRecorder.prepare();
-        } catch (IllegalStateException e) {
-            Log.d(TAG, "IllegalStateException preparing MediaRecorder: " + e.getMessage());
-            releaseMediaRecorder();
-            return false;
-        } catch (IOException e) {
-            Log.d(TAG, "IOException preparing MediaRecorder: " + e.getMessage());
-            releaseMediaRecorder();
-            return false;
-        }
-        return true;
-    }
-
-    class MediaPrepareTask extends AsyncTask<Void, Void, Boolean> {
-
-        @Override
-        protected Boolean doInBackground(Void... voids) {
-            // initialize video camera
-            if (prepareVideoRecorder()) {
-                // Camera is available and unlocked, MediaRecorder is prepared,
-                // now you can start recording
-                mMediaRecorder.start();
-
-                isRecording = true;
-            } else {
-                // prepare didn't work, release the camera
-                releaseMediaRecorder();
-                return false;
-            }
-            return true;
+            mCamera = Camera.open(findFrontFacingCamera());
+        } catch (Error e) {
+            mCamera.release();
+            e.printStackTrace();
         }
 
-        @Override
-        protected void onPostExecute(Boolean result) {
-            if (!result) {
-                Log.d(TAG, "Done with VideoActivity!");
-                VideoActivity.this.finish();
-            }
-        }
+        mMediaRecorder = new MediaRecorder();
+        mMediaRecorder.setCamera(mCamera);
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP);
+        mMediaRecorder.setMaxDuration((int) 60000);
+        CamcorderProfile camcorderProfile_HQ = CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH);
+        mMediaRecorder.setProfile(camcorderProfile_HQ);
+        mMediaRecorder.setOutputFile(getPath());
+
+        mMediaRecorder.prepare();
+        mMediaRecorder.start();
     }
 
 }
